534593a7137736d25b5eec5ce2b18d9bf06d414b
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2023-04-18 16:36:15 -0400

    add input component and working parsing function

--- src/App.js
+++ src/App.js
@@ -1,36 +1,109 @@
 import './App.css';
+import {useState} from 'react'
 import {Input} from './components/Input'
 import Select from './components/Select'
 
 function App() {
+  const [converted, setConverted] = useState('')
+  const [amount, setAmount] = useState('')
+
+  function parseValueAndUnit(str) {
+    const parts = str.trim().split(' ');
+    if (parts.length < 2) {
+      alert('Iinput a value and a unit type')
+      return null;
+    }
+    if(!/\d/.test(parts[0])){
+      alert('Input a value and a unit type')
+      return null;
+    };
+    const unit = parts.pop();
+    console.log(parts)
+    let value = 0;
+    if (parts.length === 1) {
+      console.log('length of 1 is running')
+      if(parts[0].includes('/')){
+        console.log('/ included is running')
+      value = parseFloat(parts[0].split('/').reduce((a,c)=> a/c).toFixed(3))  
+      }else{
+        console.log('/ not included is running')
+
+        value = parseFloat(parts[0])
+      };
+    } else if (parts.length === 2) {
+        const [intPart, fracPart] = parts
+        console.log(`int: ${intPart}, frac: ${fracPart}`)
+        if (fracPart) {
+            const [numerator, denominator] = fracPart.split('/');
+            console.log(`numerator: ${numerator}, denom: ${denominator}`)
+            value = parseFloat(intPart) + parseFloat(numerator) / parseFloat(denominator);
+            console.log(value)
+          	value = parseFloat(value.toFixed(3))
+            console.log(value)
+        } else {
+            value = parseFloat(intPart);
+        }
+    } else {
+      return alert('Input amount and unit type');
+    }
+    if (isNaN(value)) {
+        throw new Error(`Invalid input string: "${str}"`);
+    }
+    return {
+        value,
+        unit
+    };
+}
+  
   function handleClick(){
-    console.log(document.getElementById('amount').value)
+    const solution = document.querySelector('#solution')
+    const amount = document.querySelector('#amount').value
+    if(parseValueAndUnit(amount)){
+      const {value, unit} = parseValueAndUnit(amount)
+      console.log('you are here', value, unit)
+      
+      solution.innerText = `Solution: ${value} ${unit}`
+    }
   }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(event)
+    console.log('form submitted ✅')
+  }
+
+
   return (
     <>
-      <div className="App flex-auto">
-        <div>
-          <Input  name='amount'
-            type='text'
+      <div className="h-full">
+        <form onSubmit={handleSubmit} className="flex items-center justify-around content-center h-1/3">
+          <div>
+            <Input  name='amount'
+              type='text'
+              />
+            {/* <Input name='unit'
+              type='text'
+              /> */}
+          </div>
+          <div className="w-1/3">
+            <h2 className="text-xl">To:</h2>
+            <Select className='text-xl' name='newUnit'
+            options={[
+              'g',
+              'mg',
+              'kg',
+              'oz',
+              'lb',
+              ]}
             />
-          <Input name='unit'
-            type='text'
-            />
-        </div>
-        <h2>To:</h2>
-        <Select name='newUnit'
-        options={[
-          'g',
-          'mg',
-          'kg',
-          'oz',
-          'lb',
-          ]}
-        />
+          </div>
+          <input type='submit' onClick={handleClick} value="Submit" className="btn"></input>
+        </form>
+        
       </div>
-      <button  onClick={handleClick} value="Submit" className="btn" />
+      <h1 className='text-center text-3xl' id='solution'>Solution:</h1>
     </>
-      
+    
     
   );
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,14 @@
 import { capitalize } from "../utils/capitalize"
+// import { submitOnEnter } from "../utils/submitOnEnter"
 
 export function Input({name, type}){
 
     return(
-        <div className="form-control w-full max-w-xs">
+        <div className="form-control  max-w-xs">
             <label className="label">
-            <span className="label-text">{capitalize(name)}:</span>
+            <span className="label-text">{capitalize(name)}:{type === 'text' && '(space between amount and unit)'}</span>
             </label>
-            <input type={type} id={name} placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+            <input type={type} id={name} placeholder="Type here" className="input input-bordered  max-w-xs" />
         </div>
     )
 }
